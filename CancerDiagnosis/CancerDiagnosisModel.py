539fdb542159b574f947d7478e5270daab2675f0
Moustafa <farhat.forall@gmail.com>
2019-05-18 15:12:16 +0200

    Import libraries

--- CancerDiagnosis/CancerDiagnosisModel.py
+++ CancerDiagnosis/CancerDiagnosisModel.py
@@ -1,1 +1,7 @@
-
+#Import important libraries
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
