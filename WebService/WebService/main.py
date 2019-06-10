6696cb4d930bb0b2840c18f8e5ac375ae1ed8aa2
Moustafa <farhat.forall@gmail.com>
2019-06-11 00:59:34 +0200

    Files added

---
+++ WebService/WebService/main.py
@@ -0,0 +1,14 @@
+# main.py
+
+from flask import Blueprint
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return 'Index'
+
+@main.route('/profile')
+def profile():
+    return 'Profile'
