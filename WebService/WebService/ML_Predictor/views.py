ebefa9a5957a48bd81c0190dc0eaf127846608f5
Moustafa <farhat.forall@gmail.com>
2019-06-27 02:03:13 +0200

    Small changes

--- WebService/WebService/ML_Predictor/views.py
+++ WebService/WebService/ML_Predictor/views.py
@@ -16,22 +16,21 @@ Predictor_blueprint = Blueprint('Predictor', __name__)
 
 #ML_Model_Cancer_Predictor 1  function
 def M_ValuePredictor1(to_predict_list):
+
     loaded_model = pickle.load(open("SerializedModel/model.pkl","rb"))
-    result = loaded_model.predict(to_predict_list)
+    # load the model from disk
+
+    result = loaded_model.predict([to_predict_list])
 
     # creating and training a model
     # serializing our model to a file called model.pkl
 
-    pickle.dump(lr, open('model.pkl', "wb"))
-
-    # load the model from disk
-    loaded_model = pickle.load(open('model.pkl', 'rb'))
     #result = loaded_model.predict(data_df[predictor_lst])
-    result2= loaded_model.predict([[1, 1, 2, 5,
-                       1, 1, 5]])
-    print(result2)
+    #result2= loaded_model.predict([[1, 1, 2, 5,1, 1, 5]])
 
-    return result[0]
+
+    #print(result2)
+    return result2
 
 #ML_Model_Cancer_Predictor 2  function
 def M_ValuePredictor2(to_predict_list):
@@ -43,27 +42,34 @@ def M_ValuePredictor2(to_predict_list):
 
 #Get result from serialized Models
 @app.route('/index',methods = ['POST'])
-def result():
+def result(data):
     if request.method == 'POST':
-        to_predict_list = request.form.to_dict()
-        to_predict_list=list(to_predict_list.values())
-        to_predict_list = list(map(int, to_predict_list))
+
+        result = M_ValuePredictor1(data)
+
+        Form = PredictionForm(request.form)
+
+        Form.result.data = str(result)
+
+        #to_predict_list = request.form.to_dict()
+        #to_predict_list=list(to_predict_list.values())
+        #to_predict_list = list(map(int, to_predict_list))
 
         #Get the predicted value from the first Model
-        predictor1 = M_ValuePredictor1(to_predict_list)
+        #predictor1 = M_ValuePredictor1(to_predict_list)
 
         #Get the predicted value from the Second  Model
-        predictor2 = M_ValuePredictor1(to_predict_list)
+        #predictor2 = M_ValuePredictor1(to_predict_list)
 
         #Compare results to get more Accuracy
-        if predictor1==prepredictor2:
-                    if int(result)==2:
-                        prediction='Cancer'
-                    else:
-                        prediction='No Cancer'
+        #if predictor1==prepredictor2:
+       #             if int(result)==2:
+      #                  prediction='Cancer'
+      #              else:
+      #                  prediction='No Cancer'
 
-        else:
+     #   else:
             #Do something else
-            pass
+     #       pass
    
-    return render_template("index.html",prediction=prediction)
+    return render_template("index.html",form =Form)
