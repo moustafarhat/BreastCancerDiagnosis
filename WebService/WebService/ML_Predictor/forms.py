1e294c6f9cdadf76d0b61092f71d69e215cc8e5d
Moustafa <farhat.forall@gmail.com>
2019-07-10 20:04:04 +0200

    ML Predictor edited

--- WebService/WebService/ML_Predictor/forms.py
+++ WebService/WebService/ML_Predictor/forms.py
@@ -1,9 +1,22 @@
 #################
 #### imports ####
 #################
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_required
+from ..models import User , Patient, db,Patient_Informations
+from WebService import app
 from flask_wtf import Form
-from flask_wtf import Form
-from wtforms import DateField, TextField
+from wtforms import DateField, TextField, DecimalField, HiddenField, validators
+from wtforms.fields.html5 import TelField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+from datetime import date, datetime
+
 
 class PredictionForm(Form):
-    result = TextField()
+    clump_thickness = TextField('clump_thickness', validators=[DataRequired()])
+    uniformity_cell_size = TextField('uniformity_cell_size', validators=[DataRequired()])
+    uniformity_cell_shape = TextField('uniformity_cell_shape', validators=[DataRequired()])
+    marginal_adhesion = TextField('marginal_adhesion', validators=[DataRequired()])
+    single_epithelial_cell_size = TextField('single_epithelial_cell_size', validators=[DataRequired()])
+    bland_chromatin = TextField('bland_chromatin', validators=[DataRequired()])
+    normal_nucleoli = TextField('normal_nucleoli', validators=[DataRequired()])
--- WebService/WebService/ML_Predictor/views.py
+++ WebService/WebService/ML_Predictor/views.py
@@ -3,9 +3,18 @@
 #################
 
 from flask import render_template, Blueprint
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn import model_selection
+from ..models import Patient_Informations, db
 from WebService import app
 from .forms import *
 import pickle
+import sys
+
 
 ################
 #### config ####
@@ -13,11 +22,14 @@ import pickle
 
 Predictor_blueprint = Blueprint('Predictor', __name__)
 
+##########################
+#### helper functions ####
+##########################
 
 #ML_Model_Cancer_Predictor 1  function
 def M_ValuePredictor1(to_predict_list):
 
-    loaded_model = pickle.load(open("SerializedModel/model.pkl","rb"))
+    loaded_model = pickle.load(open("SerializedModel/ML_Model_Cancer_Predictor1.pkl","rb"))
     # load the model from disk
 
     result = loaded_model.predict([to_predict_list])
@@ -30,7 +42,7 @@ def M_ValuePredictor1(to_predict_list):
 
 
     #print(result2)
-    return result2
+    return result
 
 #ML_Model_Cancer_Predictor 2  function
 def M_ValuePredictor2(to_predict_list):
@@ -41,16 +53,39 @@ def M_ValuePredictor2(to_predict_list):
 
 
 #Get result from serialized Models
-@app.route('/index',methods = ['POST'])
+@app.route('/index', methods=["GET", "POST"])
 def result(data):
-    if request.method == 'POST':
 
-        result = M_ValuePredictor1(data)
+    # Data that we need for Model (it is important to follow this order!)
+    #[0] clump_thickness
+    #[1] uniformity_cell_size
+    #[2] uniformity_cell_shape
+    #[3] marginal_adhesion
+    #[4] single_epithelial_cell_size
+    #[5] bland_chromatin
+    #[6] normal_nucleoli
 
-        Form = PredictionForm(request.form)
+     if request.method == "POST":
 
-        Form.result.data = str(result)
+        form = PredictionForm(request.form)
 
+        clump_thickness = request.form.get('clump_thickness')
+        uniformity_cell_size = request.form.get('uniformity_cell_size')
+        uniformity_cell_shape = request.form.get('uniformity_cell_shape')
+        marginal_adhesion = request.form.get('marginal_adhesion')
+        single_epithelial_cell_size = request.form.get('single_epithelial_cell_size')
+        bland_chromatin = request.form.get('bland_chromatin')
+        normal_nucleoli = request.form.get('normal_nucleoli')
+
+        try:
+
+           result = M_ValuePredictor1([clump_thickness,uniformity_cell_size,uniformity_cell_shape,marginal_adhesion,single_epithelial_cell_size,bland_chromatin,normal_nucleoli])
+
+        except Exception as e: 
+            
+            print(e)
+
+        #TODO::In future we will compare result from differents Models
         #to_predict_list = request.form.to_dict()
         #to_predict_list=list(to_predict_list.values())
         #to_predict_list = list(map(int, to_predict_list))
@@ -72,4 +107,4 @@ def result(data):
             #Do something else
      #       pass
    
-    return render_template("index.html",form =Form)
+     return render_template("index.html",form =Form)
